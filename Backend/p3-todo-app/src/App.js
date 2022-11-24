b394476bfd7b1414a4ee79c2f0be78f9a174939a
GuiGonDi925 <106049639+GuiGonDi925@users.noreply.github.com>
2022-11-24 07:15:55 -0600

    P1-react
    primera parte "to do" con react

---
+++ Backend/p3-todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Container from "./components/Container"
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+          <Container />
+      </header>
+    </div>
+  );
+}
+
+export default App;
